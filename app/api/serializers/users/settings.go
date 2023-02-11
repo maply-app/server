141e53d671a841e426381bf141cad300e19d6fed
ValeryChapman <valeryparkhomenkomain@gmail.com>
2023-02-11 12:43:52 +0300

    [UPDATED] avatars logic & jwt

--- app/api/serializers/users/settings.go
+++ app/api/serializers/users/settings.go
@@ -1,7 +1,6 @@
 package users
 
 import (
-	"fmt"
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
 	"maply/models"
@@ -10,13 +9,7 @@ import (
 func SettingsSerializer(c *fiber.Ctx) (*models.Settings, bool) {
 	data := &models.Settings{}
 	if err := c.BodyParser(data); err != nil {
-		fmt.Printf("err –> %s", err)
 		return data, false
-	}
-
-	avatar, err := c.FormFile("avatar")
-	if err == nil {
-		data.Avatar = avatar
 	}
 
 	validate := validator.New()
--- app/models/settings.go
+++ app/models/settings.go
@@ -3,5 +3,5 @@ package models
 type Settings struct {
 	Name     string `json:"name" validate:"omitempty,min=2,max=24"`
 	Username string `json:"username" validate:"omitempty,min=4,max=24"`
-	Avatar   string `json:"avatar" validate:",min=4,max=24omitempty"`
+	Avatar   string `json:"avatar" validate:"omitempty,min=32,max=128"`
 }
